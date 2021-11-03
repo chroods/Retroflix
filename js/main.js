1cb5348d6e7c0d58db291d24ea7fac10615599fa
david wanderson <daviddonnna@hotmail.com>
2021-11-02 22:10:35 -0300

    Adicionando sub pastas

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+$('.owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:7
+        }
+    }
+})
